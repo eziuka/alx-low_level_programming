6afca5a59476615e5b2598fd913dcdc79efc37b5
eziuka <eziukajustice@gmail.com>
2023-02-24 01:07:09 -0800

    isupper

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,15 +1,15 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-int _putchar(char c);
-
 int _isupper(int c);
 
 int _isdigit(int c);
 
 int mul(int a, int b);
 
-void print_number(void);
+void print_numbers(void);
+
+int _putchar(char c);
 
 void print_most_numbers(void);
 

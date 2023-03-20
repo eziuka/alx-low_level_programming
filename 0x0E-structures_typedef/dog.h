38a8cc2799ab2f19b5569ac50ccf417f74d9081c
eziuka <eziukajustice@gmail.com>
2023-03-20 11:14:45 -0700

    0. Poppy

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,19 +1,31 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
+
 #include <stdio.h>
-#include "dog.h"
+#include <stdlib.h>
 
 /**
- * main - check the code
- *
- * Return: Always 0.
+ * struct dog - blueprint for a dog instance
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: dog owner's name
+ * Description: dog struct
  */
-int main(void)
+struct dog
 {
-    struct dog my_dog;
+	char *name;
+	float age;
+	char *owner;
+};
 
-    my_dog.name = "Poppy";
-    my_dog.age = 3.5;
-    my_dog.owner = "Bob";
-    printf("My name is %s, and I am %.1f :) - Woof!\n", my_dog.name, my_dog.age);
-    return (0);
-}
+typedef struct dog dog_t;
 
+void init_dog(struct dog *d, char *name, float age, char *owner);
+
+void print_dog(struct dog *d);
+
+dog_t *new_dog(char *name, float age, char *owner);
+
+void free_dog(dog_t *d);
+
+#endif /* _MAIN_H_ */
